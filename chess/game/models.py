80334c30e630df498edfb6aaa313555be338398d
Lucas Brambrink <lucasbrambrink@Lucass-MacBook-Pro.local>
2017-06-20 08:25:59 -0400

    builds out available step discovery

--- chess/game/models.py
+++ chess/game/models.py
@@ -1,10 +1,15 @@
 from django.db import models
-
+from .utils import *
 # Create your models here.
 
 
+class Game(object):
 
-
-class Board(models.Model):
-    board = models.
+    def __init__(self):
+        self.board = Board()
+        self.square = Square('a', 1)
+        self.pawn = Pawn(self.square, Piece.WHITE)
+        self.bishop = Bishop(Square('c', 2), Piece.WHITE)
+        self.rook = Rook(Square('g', 8), Piece.WHITE)
+        self.queen = Queen(Square('e', 4), Piece.WHITE)
 
--- chess/game/utils.py
+++ chess/game/utils.py
@@ -2,16 +2,16 @@
 
 class Square(object):
     FILES = 'abcdefgh'
-    RANK = tuple(range(8))
+    RANK = tuple(range(1, 9))
     NULL_FILE = 'z'
 
-    def __init__(self, rank, file, piece=None):
-        self.rank = rank
+    def __init__(self, file, rank, piece=None):
         self.file = file
+        self.rank = rank
         self.piece = piece
 
     def __repr__(self):
-        return '%s%s' % (self.file, self.rank + 1)
+        return '%s%s' % (self.file, self.rank)
 
     @property
     def is_out_of_bounds(self):
@@ -22,34 +22,48 @@ class Square(object):
         return self.piece is None
 
     def step(self, step):
-        rank = self.rank + step[0]
-        file_index = self.FILES.index(self.file) + step[1]
-        file = self.NULL_FILE if file_index not in self.RANK \
-            else self.FILES[file_index]
-        return self.__class__(rank, file)
+        rank = self.rank + step[1]
+
+        try:
+            file_index = self.FILES.index(self.file) + step[0]
+            if file_index < 0:
+                raise IndexError()
+            file = self.FILES[file_index]
+        except (ValueError, IndexError):
+            file = self.NULL_FILE
+
+        return self.__class__(file, rank)
 
 
 class Board(object):
     """dict of files"""
 
     def __init__(self):
-        self.position = {f: [Square(r, f) for r in Square.RANK]
+        self.position = {f: [Square(f, r) for r in Square.RANK]
                          for f in Square.FILES}
 
     def __repr__(self):
-        return self.position
+        return str(self.position)
 
     def __getitem__(self, item):
+        square = None
         try:
             ranks = self.position[item.file]
-            return ranks[item.rank]
-        except KeyError:
-            return None
+            square = ranks[item.rank - 1]
+        except (IndexError, KeyError):
+            pass
+        return square
 
+    def __setitem__(self, key, value):
+        assert isinstance(key, Square)
+        assert isinstance(value, Piece)
+        square = self[key]
+        square.piece = value
 
 
 class Piece(object):
     steps = []
+    relative_value = 0
     can_travel = False
     WHITE = 'W'
     BLACK = 'B'
@@ -65,19 +79,16 @@ class Piece(object):
         new_position = position.step(step)
         board_position = board[new_position]
         cant_step = board_position is None \
-            or new_position.is_out_of_bounds \
-            or (not board_position.is_empty
-                and board_position.piece.COLOR == self.color)
+                    or new_position.is_out_of_bounds \
+                    or (not board_position.is_empty
+                        and board_position.piece.COLOR == self.color)
 
-        steps = []
-        if not cant_step:
-            steps.append(new_position)
+        if not self.can_travel:
+            return [] if cant_step else [new_position]
 
-        if self.can_travel:
-            steps.extend(self.take_step(board, new_position, step))
-
-        return steps
-
+        # step recursively until direction is exhausted
+        return [] if cant_step \
+            else [new_position] + self.take_step(board, new_position, step)
 
     def available_steps(self, board):
         available_steps = []
@@ -92,31 +103,37 @@ class Piece(object):
 
 class Pawn(Piece):
     steps = [(0, 1)]
+    relative_value = 1
     can_travel = False
 
 class Knight(Piece):
+    relative_value = 3
     steps = [(2, 3), (-2, 3), (2, -3), (-2, -3),
              (3, 2), (-3, 2), (3, -2), (-3, -2)]
     can_travel = False
 
 
 class Bishop(Piece):
+    relative_value = 3
     steps = [(1, 1), (-1, 1), (-1, -1), (1, -1)]
     can_travel = True
 
 
 class Rook(Piece):
+    relative_value = 5
     steps = [(1, 0), (-1, 0), (0, 1), (0, -1)]
     can_travel = True
 
 
 class King(Piece):
+    relative_value = 100
     steps = [(1, 1), (0, 1), (-1, 1), (-1, 0), (-1, -1),
              (0, -1), (1, -1), (1, 0), (1, 1), ]
     can_travel = False
 
 
 class Queen(Piece):
+    relative_value = 9
     steps = [(1, 1), (0, 1), (-1, 1), (-1, 0), (-1, -1),
              (0, -1), (1, -1), (1, 0), (1, 1),]
     can_travel = True
